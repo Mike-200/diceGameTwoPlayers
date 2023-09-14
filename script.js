7f71240223df945988e1e8925a724e2d7a4fbeec
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-14 13:31:06 +0100

    array variables reset to make them more dynamic to allow for varying numbers of players

--- script.js
+++ script.js
@@ -1,113 +1,102 @@
-// connect to html id's, classes, etc
 const diceImage = [];
-diceImage[1] = document.getElementById("diceImage1");
-diceImage[2] = document.getElementById("diceImage2");
-
 const displayScore = [];
-displayScore[1] = document.getElementById("displayScore1");
-displayScore[2] = document.getElementById("displayScore2");
-
 const rollDiceButton = [];
-rollDiceButton[1] = document.getElementById("rollDiceButton1");
-rollDiceButton[2] = document.getElementById("rollDiceButton2");
-
 const rolls = [];
-rolls[1] = document.getElementById("rolls1");
-rolls[2] = document.getElementById("rolls2");
-
 const rollTheDiceText = [];
-rollTheDiceText[1] = document.getElementById("rollTheDiceText1");
-rollTheDiceText[2] = document.getElementById("rollTheDiceText2");
-
 const holdButton = [];
-holdButton[1] = document.getElementById("holdButton1");
-holdButton[2] = document.getElementById("holdButton2");
+
+const numberOfPlayers = 2;
+
+for (let i = 1; i <= numberOfPlayers; i++) {
+  diceImage[i] = document.getElementById(`diceImage${i}`);
+  displayScore[i] = document.getElementById(`displayScore${i}`);
+  rollDiceButton[i] = document.getElementById(`rollDiceButton${i}`);
+  rolls[i] = document.getElementById(`rolls${i}`);
+  rollTheDiceText[i] = document.getElementById(`rollTheDiceText${i}`);
+  holdButton[i] = document.getElementById(`holdButton${i}`);
+}
 
 const minRolls = document.getElementById("minRolls");
-const targetPoints = document.getElementById("targetPoints");
 
-const numberOfPlayers = 1;
 const pointsToReach = 20;
+const targetPoints = document.getElementById("targetPoints");
+targetPoints.textContent = pointsToReach;
+
+let minimumNumberOfRolls = 10;
 let score = [];
 let numberOfRolls = [];
-let minimumNumberOfRolls = 10;
 let playerNumber = 1;
 // let playerNumber = Math.ceil(Math.random() * 2);
 
-targetPoints.textContent = pointsToReach;
-
 const holdButtonPressed = () => {
-  console.log("hold button pressed");
+  console.log(`hold button for player ${playerNumber} pressed`);
   // store the score
   // pass control to the other player
+  changePlayerNumber();
+};
+holdButton[1].addEventListener("click", holdButtonPressed);
+holdButton[2].addEventListener("click", holdButtonPressed);
+
+const changePlayerNumber = () => {
   playerNumber++;
   if (playerNumber > numberOfPlayers) {
     playerNumber = 1;
   }
+  //console.log(`player number ${playerNumber} is now in play`);
 };
 
-holdButton[1].addEventListener("click", holdButtonPressed);
-holdButton[2].addEventListener("click", holdButtonPressed);
-
 const resetGame = () => {
-  score[playerNumber] = 0;
-  displayScore[1].textContent = score[playerNumber];
-  displayScore[2].textContent = score[playerNumber];
+  for (let i = 1; i <= numberOfPlayers; i++) {
+    diceImage[i].src = "./images/dice3d.png";
+    numberOfRolls[i] = 0;
+    rolls[i].textContent = numberOfRolls[playerNumber];
+    score[i] = 0;
+    displayScore[i].textContent = score[playerNumber];
+  }
+  for (let i = 1; i <= numberOfPlayers; i++) {
+    changePlayerNumber();
 
-  numberOfRolls[playerNumber] = 0;
-  rolls[1].textContent = numberOfRolls[playerNumber];
-  rolls[2].textContent = numberOfRolls[playerNumber];
-
-  diceImage[1].src = "./images/dice3d.png";
-  diceImage2.src = "./images/dice3d.png";
-
-  swapRollButton("Roll");
+    swapRollButton("Roll");
+  }
 };
 
 const rollTheDice = () => {
-  //const diceImage = eval(`diceImage${playerNumber}`);
-  // generate a random number 1 to 6
   const diceRoll = Math.ceil(Math.random() * 6);
-  //const rolls = eval(`rolls${playerNumber}`);
   numberOfRolls[playerNumber]++;
-
   setTimeout(() => {
-    //let diceImage = JSON.parse("diceImage1");
-    //console.log(typeof diceImage);
-    //console.log(typeof diceImage1);
-    diceImage[1].style.animation = "none";
-    diceImage[1].src = "./images/dice" + diceRoll + ".png";
-    rolls[1].textContent = numberOfRolls[playerNumber];
-    //diceImage2.style.animation = "none";
-
-    //rolls2.textContent = numberOfRolls[playerNumber];
-
-    //diceImage2.src = "./images/dice" + diceRoll + ".png";
-
+    diceImage[playerNumber].style.animation = "none";
+    diceImage[playerNumber].src = "./images/dice" + diceRoll + ".png";
+    rolls[playerNumber].textContent = numberOfRolls[playerNumber];
     // check if a 1 has been rolled
     if (diceRoll === 1) {
-      swapRollButton("Play again");
+      //swapRollButton("Play again");
+      score[playerNumber] = 0;
+      displayScore[playerNumber].textContent = score[playerNumber];
+
+      changePlayerNumber();
     } else {
       score[playerNumber] += diceRoll;
-      displayScore[1].textContent = score[playerNumber];
-      displayScore[2].textContent = score[playerNumber];
-
+      displayScore[playerNumber].textContent = score[playerNumber];
+      // check for a winner
       if (score[playerNumber] > pointsToReach) {
+        console.log(`player ${playerNumber} is the winner`);
         checkIfRecordSet();
+        swapRollButton();
+        //resetGame()
       }
     }
   }, 0); // Math.round(Math.random() * 3000) + 1000);
-  diceImage[1].src = "./images/dice3d.png";
+  diceImage[playerNumber].src = "./images/dice3d.png";
   //diceImage2.src = "./images/dice3d.png";
 
-  diceImage[1].style.animation = "jump-shaking 0.83s infinite";
+  diceImage[playerNumber].style.animation = "jump-shaking 0.83s infinite";
   //diceImage2.style.animation = "jump-shaking 0.83s infinite";
 };
 
 const checkIfRecordSet = () => {
   if (numberOfRolls[playerNumber] < minimumNumberOfRolls) {
-    rollTheDiceText[1].textContent = "NEW RECORD SET - WELL DONE !";
-    rollTheDiceText[2].textContent = "NEW RECORD SET - WELL DONE !";
+    rollTheDiceText[playerNumber].textContent = "NEW RECORD SET - WELL DONE !";
+    rollTheDiceText[playerNumber].style.opacity = 1;
 
     minimumNumberOfRolls = numberOfRolls[playerNumber];
     minRolls.textContent = minimumNumberOfRolls;
@@ -121,26 +110,25 @@ const checkIfRecordSet = () => {
 
 // this funtions will need splitting
 const swapRollButton = (buttonFunctionality) => {
-  rollDiceButton1.textContent = buttonFunctionality;
+  // Roll or Hold visible
   if (buttonFunctionality === "Roll") {
-    rollTheDiceText[1].style.opacity = 0;
-    rollTheDiceText[2].style.opacity = 0;
-
-    rollDiceButton[1].removeEventListener("click", resetGame);
-    rollDiceButton[2].removeEventListener("click", resetGame);
-
-    rollDiceButton[1].addEventListener("click", rollTheDice);
-    rollDiceButton[2].addEventListener("click", rollTheDice);
-  } else if (buttonFunctionality === "Play again") {
-    rollTheDiceText[1].textContent = "GAME OVER !";
-    rollTheDiceText[2].textContent = "GAME OVER !";
-
-    rollDiceButton[1].removeEventListener("click", rollTheDice);
-    rollDiceButton[2].removeEventListener("click", rollTheDice);
-
-    rollDiceButton[1].addEventListener("click", resetGame);
-    rollDiceButton[2].addEventListener("click", resetGame);
+    rollDiceButton[playerNumber].textContent = buttonFunctionality;
+    rollTheDiceText[playerNumber].style.opacity = 1;
+    rollDiceButton[playerNumber].removeEventListener("click", resetGame);
+    rollDiceButton[playerNumber].addEventListener("click", rollTheDice);
+  } else if (buttonFunctionality === "Nothing Visible") {
+    rollDiceButton[playerNumber].textContent = buttonFunctionality;
+    rollTheDiceText[playerNumber].style.opacity = 1;
+    rollDiceButton[playerNumber].removeEventListener("click", resetGame);
+    rollDiceButton[playerNumber].removeEventListener("click", rollTheDice);
+  } else if (buttonFunctionality === "Start New Game!") {
+    rollDiceButton[playerNumber].textContent = buttonFunctionality;
+    for (let i = 0; i < numberOfPlayers; i++) {
+      rollTheDiceText[i].textContent = `PLAYER ${playerNumber} WINS !`;
+      rollTheDiceText[playerNumber].style.opacity = 1;
+      rollDiceButton[playerNumber].removeEventListener("click", resetGame);
+      rollDiceButton[playerNumber].addEventListener("click", rollTheDice);
+    }
   }
 };
-
 resetGame();
